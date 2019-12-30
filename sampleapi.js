d3a8f8e63de17ebbb67fb8b48096471cebf95d54
Ashish S <ashishjsharda@gmail.com>
2019-12-30 18:06:36 -0500

    Create sampleapi.js

---
+++ sampleapi.js
@@ -0,0 +1,15 @@
+var http=require('http')
+const server=http.createServer((req,res)=>{
+   if(req.url==='/')
+   {
+      res.write('<h1>Hello World</h1>');
+      res.end();
+   }
+   if(req.url==='/api/catalog')
+   {
+      res.write(JSON.stringify([1,2,3]));
+      res.end();
+   }
+});
+server.listen(3000);
+console.log("Listening on port 3000");
