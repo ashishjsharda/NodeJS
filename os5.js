3653278d5fa42838696ef0d79b072cd93c595bd9
Ashish S <ashishjsharda@gmail.com>
2020-09-01 16:36:50 -0400

    Create os5.js

---
+++ os5.js
@@ -0,0 +1,6 @@
+var os =require("os")
+console.log(os.endianness());
+console.log("Type seen is ",os.type());
+console.log("Platform seen is ",os.platform());
+console.log("Total memory seen is ",os.freemem());
+console.log("Total memory seen is ",os.totalmem());
